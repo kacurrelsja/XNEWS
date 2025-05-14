cd7d9a62bf7050a10b5c3e32d6836eb4e81f3647
kacurrelsja <palajxhulia13@gmail.com>
2025-05-14 15:27:24 +0200

    rregullimet e fundit

--- next.config.js
+++ next.config.js
@@ -1,31 +1,34 @@
 /* eslint-disable no-unused-vars */
-const { i18n } = require("./next-i18next.config");
+const { i18n: i18nConfig } = require("./next-i18next.config");
 const path = require("path");
 
-module.exports = { 
-   
-    i18n,
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, "./src/assets/scss")],
-        silenceDeprecations: ["legacy-js-api"],
-    },
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'coin-images.coingecko.com',
-          },
-        ],
+module.exports = {
+  i18n: {
+    locales: ['en', 'it'],
+    defaultLocale: 'it',
+    localeDetection: false
+  },
+
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "./src/assets/scss")],
+    silenceDeprecations: ["legacy-js-api"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
       },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // eslint-disable-next-line no-param-reassign
-        config.ignoreWarnings = [
-            {
-                message:
-                    /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
-            },
-        ];
-        return config;
-    },
+    ],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.ignoreWarnings = [
+      {
+        message:
+          /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
+      },
+    ];
+    return config;
+  },
 };
